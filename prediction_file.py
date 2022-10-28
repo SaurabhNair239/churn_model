a0148b7bac0353f3ffd66a984f189e75f1024360
SaurabhNair239 <58506467+SaurabhNair239@users.noreply.github.com>
2022-10-28 09:51:27 +0200

    Update prediction_file.py

--- prediction_file.py
+++ prediction_file.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 import pickle
 def predict_cnn(data):
-    model = tf.keras.models.load_model("F:\Project\churn_model\churn_model_file.h5")
-    scalar_model = pickle.load(open("F:\Project\churn_model\sc_model.pickle", 'rb'))
+    model = tf.keras.models.load_model("churn_model_file.h5")
+    scalar_model = pickle.load(open("sc_model.pickle", 'rb'))
     data_scaled = scalar_model.transform(data)
     y_pred = model.predict(data_scaled)
     y_pred = (y_pred >= 0.5)
